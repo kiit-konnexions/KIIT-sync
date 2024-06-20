02eda6a8ee0e4e26a6fcbd5fb852e3a6d969c626
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-21 02:31:40 +0530

    Update tailwind.config.js with responsive screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,25 +12,20 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+
+        xxl : { min: "1439px" },
+
+        xl: { min: "1279px" },
+
+        lg: { min: "1023px" },
+
+        md: { min: "767px" },
+
+        sm: { min: "639px" },
+      },
     },
   },
   plugins: [],
-  theme: {
-    screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
 
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
-      md: { max: "770px" },
-      // => @media (max-width: 767px) { ... }
-
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
-    },
-  },
 };
