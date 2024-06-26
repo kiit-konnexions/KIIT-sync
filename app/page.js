f2c29771f981f57b3a0e127b00747cca3b2adbce
AvaterClasher <avater.clasher@gmail.com>
2024-06-27 00:11:16 +0530

    fix: adjusted the page.js

--- app/page.js
+++ app/page.js
@@ -1,28 +1,13 @@
-import Card from "./components/Card";
-import Contributor from "./components/Contributor";
-import Footer from "./components/Footer";
+import GradientItems from "./components/GradientItems";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
 import Features from "./components/features/feature";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <div className="flex gap-5 w-full justify-center items-center sm:px-3">
-        <Card
-          iconSrc={"/Student.jpeg"}
-          title="Students"
-          bgColor="bg-green-100"
-        />
-        <Card
-          iconSrc={"/teacher.png"}
-          title="Teachers"
-          bgColor="bg-slate-100"
-        />
-      </div>
+      <GradientItems/>
       <Hero />
-      <Contributor />
       <Features />
       <Footer />
     </>
