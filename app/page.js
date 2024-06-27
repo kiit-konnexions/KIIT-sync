983a9b165c3199962e8dd52bf4491f84fa9cd658
Soumyadip Moni <116944847+AvaterClasher@users.noreply.github.com>
2024-06-27 20:22:07 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import GradientItems from "./components/GradientItems";
 import Hero from "./components/Hero";
+import Features from "./components/features/feature";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <GradientItems />
       <Hero />
+      <Features />
+      <Footer />
     </>
   );
 }
