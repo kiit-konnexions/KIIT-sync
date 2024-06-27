9999bbec1d5aa123ccc5b9a75c7f134f0044ed35
AvaterClasher <avater.clasher@gmail.com>
2024-06-27 20:01:40 +0530

    chore: deleted wrong components

--- app/components/Mockup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Mockup = () => {
-  return (
-    <div className='h-[519px] w-[1113px] rounded-[20px] mx-auto my-[75px] flex justify-center items-center bg-[#EEEEEE] text-[20px] font-medium xl:w-[950px] xl:mx-auto lg:w-[750px] lg:mx-auto md:h-[326px] md:w-[550px] md:mx-auto sm:w-[340px] sm:h-[226px] sm:mx-[4px] sm:my-[46px]'><p className='sm:hidden'>Mockup image / to be decided later</p></div>
-  )
-}
-
-export default Mockup
