7378fedfca46c87bf2cf57efeaef973b0909f3a3
Supreet0 <supreetp2004@gmail.com>
2024-06-29 00:55:57 +0530

    Navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,32 +4,35 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-between bg-white py-8 px-10">
       <div className="flex items-center">
-        <div className="mr-4">
+        <div className="mr-2">
           <img src="kiit.svg" alt="" />
         </div>
-        <div className="hidden mg:flex">
-          <span className="font-bold text-2xl ml-2">KiiT</span>
-          <sapn className="font text-2xl">Sync</sapn>
+        <div>
+          <span className="font-bold text-2xl ml-0.5">KiiT</span>
+          <sapn className="font text-2xl ml-0.5">Sync</sapn>
         </div>
       </div>
-      <div className="flex space-x-4">
-        <a href="#" className="text-black hover:text-gray-700">
+      <div className="flex space-x-10">
+        <a href="#" className="font-medium text-black  hover:text-gray-700">
           Home
         </a>
-        <a href="#" className="text-black hover:text-gray-700">
+        <a href="#" className="font-medium text-black hover:text-gray-700">
           Features
         </a>
-        <a href="#" className="text-black hover:text-gray-700">
+        <a href="#" className="font-medium text-black hover:text-gray-700">
           Contributors
         </a>
-        <a href="#" className="text-black hover:text-gray-700">
+        <a href="#" className="font-medium text-black hover:text-gray-700">
           Report an issue
         </a>
       </div>
       <div>
-        <button class="px-4 py-2 text-black rounded-md border border-slate-600 font-medium shadow-sm bg-white">
+        <button className="sticky z-50 px-4 py-2 text-black rounded-md border border-slate-300 font-medium shadow-sm bg-white hover:bg-gray-100">
           How to contribute
         </button>
+      </div>
+      <div className="lg:hidden">
+        <button className="px-4 py-2 text-black rounded-3xl border border-slate-300 font-medium shadow-sm bg-white hover:bg-gray-100">Contributors</button>
       </div>
     </nav>
   );
