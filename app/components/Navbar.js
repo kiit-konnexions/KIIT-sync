fd39019a5f2595d5608840daab3d4ec30d89bc71
Supreet0 <supreetp2004@gmail.com>
2024-07-04 14:14:31 +0530

    Navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,39 +1,19 @@
-"use client";
-import { useState, use } from "react";
+import React from "react";
 
-export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function getMenuClasses() {
-    let menuClasses = [];
-    if (isOpen) {
-      menuClasses = [
-        "flex",
-        "absolute",
-        "top-[60px]",
-        "w-full",
-        "p-10",
-        "gap-10",
-        "flex-col",
-        "left-0",
-      ];
-    } else {
-      menuClasses = ["hidden", "md:flex"];
-    }
-    return menuClasses.join(" ");
-  }
+  export default function Navbar(){
   return (
-    <nav className="flex items-center justify-between bg-white py-8 px-10">
+    <>
+    <nav className="flex items-center justify-between bg-white py-5 px-10">
       <div className="flex items-center">
-        <div className="mr-2">
+      <div className="mr-2 ">
           <img src="kiit.svg" alt="" />
         </div>
-        <div className="">
+        <div className="hidden lg:block">
           <span className="font-bold text-2xl ml-0.5">KiiT</span>
-          <sapn className="font text-2xl ml-1">Sync</sapn>
+          <sapn className="text-2xl ml-1">Sync</sapn>
         </div>
       </div>
-      <div className=" sticky z-50 flex space-x-10">
+      <div className="hidden lg:inline-flex sticky z-50  space-x-10">
         <a href="#" className="font-medium text-black  hover:text-gray-700">
           Home
         </a>
@@ -48,22 +28,19 @@ export default function Navbar() {
         </a>
       </div>
       <div>
-        <button className="sticky z-50 px-4 py-2 text-black rounded-md  border-[1.5px] border-[#D9D9D9] font-medium shadow-sm bg-white hover:bg-gray-100">
+        <button className="hidden lg:block sticky z-50 px-4 py-2 text-black rounded-md  border-[1.5px] border-[#D9D9D9] font-medium shadow-sm bg-white hover:bg-gray-100">
           How to contribute
         </button>
       </div>
-      <div className="lg:hidden flex items-center gap-4">
+      <div className="lg:hidden flex items-center gap-4 sticky z-50">
         <button className="px-4 py-2 text-black rounded-3xl  border-[1.5px] border-[#D9D9D9] font-medium shadow-sm bg-white hover:bg-gray-100">
           Contributors
         </button>
-        <button
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-        >
+        <button>
           <img src="menu.svg" alt="" />
         </button>
       </div>
     </nav>
+    </>
   );
 }
